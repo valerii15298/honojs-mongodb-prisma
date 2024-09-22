6dd9d0a261be727cc4ef8fc1b0702aebf3f6d0f7
Valerii Petryniak <valerii15298@gmail.com>
2024-09-22 14:56:35 +0300

    prisma integration

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 
+import { db } from "./prisma.js";
+
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Hono!"));
+app.get("/", async (c) => {
+  const users = await db.user.findMany();
+  return c.json(users);
+});
 
 const port = Number(process.env["PORT"]) || 4001;
-
-// eslint-disable-next-line no-console
-console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
   port,
 });
+
+// eslint-disable-next-line no-console
+console.log(`Server is running on port ${port}`);

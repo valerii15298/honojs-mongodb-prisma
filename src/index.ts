b586f4db46c364410ee3f20d2d63a65d5d00ad02
Valerii Petryniak <valerii15298@gmail.com>
2024-09-27 18:09:34 +0300

    add zones crud

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,20 @@
 import type { Http2Bindings, HttpBindings } from "@hono/node-server";
 import { serve } from "@hono/node-server";
 import { swaggerUI } from "@hono/swagger-ui";
-import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import type { PrismaClient } from "@prisma/client";
 
-import { UserSchema } from "./generated/zod/index.js";
 import { db } from "./prisma.js";
+import { zones } from "./routes/zones.js";
 
-const app = new OpenAPIHono<{
+export interface HonoCtx {
   Bindings: { db: PrismaClient } & (HttpBindings | Http2Bindings);
-}>({
+}
+const app = new OpenAPIHono<HonoCtx>({
   strict: false,
 });
 
-// get user by id
-const GetUserParamsSchema = z.object({
-  id: UserSchema.shape.id.openapi({
-    param: {
-      name: "id",
-      in: "path",
-    },
-    example: "66f5e368775d5e1f77c6749d",
-  }),
-});
-const getUserByIdRoute = createRoute({
-  method: "get",
-  path: "/users/:id",
-  request: {
-    params: GetUserParamsSchema,
-  },
-  responses: {
-    200: {
-      content: {
-        "application/json": {
-          schema: UserSchema,
-        },
-      },
-      description: "Get user by id",
-    },
-  },
-});
-app.openapi(getUserByIdRoute, async (c) => {
-  const { id } = c.req.valid("param");
-  const user = await db.user.findUniqueOrThrow({ where: { id } });
-  return c.json(user);
-});
-// get user by id end
-
-// get users
-const getUsersRoute = createRoute({
-  method: "get",
-  path: "/users",
-  responses: {
-    200: {
-      content: {
-        "application/json": {
-          schema: z.array(UserSchema),
-        },
-      },
-      description: "Get all users",
-    },
-  },
-});
-app.openapi(getUsersRoute, async (c) => {
-  const users = await db.user.findMany();
-  return c.json(users);
-});
-// get users end
-
-// post user
-const createUserRoute = createRoute({
-  method: "post",
-  path: "/users",
-  request: {
-    body: {
-      content: {
-        "application/json": {
-          schema: UserSchema.partial({ id: true }),
-        },
-      },
-    },
-  },
-  responses: {
-    200: {
-      content: {
-        "application/json": {
-          schema: UserSchema,
-        },
-      },
-      description: "Create user",
-    },
-  },
-});
-app.openapi(createUserRoute, async (c) => {
-  const data = c.req.valid("json");
-  const newUser = await db.user.create({
-    data,
-  });
-  return c.json(newUser);
-});
-// post user end
+app.route("/zones", zones);
 
 const pathOpenAPI = "/openapi";
 app.doc(pathOpenAPI, {
---
+++ src/routes/zones.ts
@@ -0,0 +1,96 @@
+import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
+
+import { ZoneSchema } from "../generated/zod/index.js";
+import type { HonoCtx } from "../index.js";
+import { db } from "../prisma.js";
+
+export const zones = new OpenAPIHono<HonoCtx>();
+
+// get zone by id
+const GetZoneByIdParamsSchema = z.object({
+  id: ZoneSchema.shape.id.openapi({
+    param: {
+      name: "id",
+      in: "path",
+    },
+    example: "66f5e368775d5e1f77c6749d",
+  }),
+});
+
+zones.openapi(
+  createRoute({
+    method: "get",
+    path: "/:id",
+    request: {
+      params: GetZoneByIdParamsSchema,
+    },
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: ZoneSchema,
+          },
+        },
+        description: "get zone by id",
+      },
+    },
+  }),
+  async (c) => {
+    const { id } = c.req.valid("param");
+    const zone = await db.zone.findUniqueOrThrow({ where: { id } });
+    return c.json(zone);
+  },
+);
+
+// get all zones
+zones.openapi(
+  createRoute({
+    method: "get",
+    path: "/",
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: z.array(ZoneSchema),
+          },
+        },
+        description: "get all zones",
+      },
+    },
+  }),
+  async (c) => {
+    const zones = await db.zone.findMany();
+    return c.json(zones);
+  },
+);
+
+// create zone
+const createUserRoute = createRoute({
+  method: "post",
+  path: "/",
+  request: {
+    body: {
+      content: {
+        "application/json": {
+          schema: ZoneSchema.omit({ id: true }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: ZoneSchema,
+        },
+      },
+      description: "create zone",
+    },
+  },
+});
+
+zones.openapi(createUserRoute, async (c) => {
+  const data = c.req.valid("json");
+  const newZone = await db.zone.create({ data });
+  return c.json(newZone);
+});
